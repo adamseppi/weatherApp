6c52ddcf8a62a3515798419b3765eb1066670092
adamseppi <adamseppi@yahoo.com>
2017-02-01 01:49:29 -0600

    First Commit

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherVC
+//  WeatherApp
+//
+//  Created by Adam Seppi on 2/1/17.
+//  Copyright © 2017 AdamSeppi. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var currentTempLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var currentWeatherDesc: UILabel!
+    @IBOutlet var currentWeatherImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
