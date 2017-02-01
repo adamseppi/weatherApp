9bace018ad8074f3f933b5378a3059ee663bd86d
adamseppi <adamseppi@yahoo.com>
2017-02-01 15:11:07 -0600

    Pulling and Parsing Json and adding to tableview cells.  Last step is to implement User Location Pulling

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Adam Seppi on 2/1/17.
+//  Copyright © 2017 AdamSeppi. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.swift
+//  WeatherApp
+//
+//  Created by Adam Seppi on 2/1/17.
+//  Copyright © 2017 AdamSeppi. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
